905b194e609dd572591e1c8eac4079992a67ded5
LetMeFly666 <814114971@qq.com>
2022-03-16 23:31:27 +0800

    g++ main.cpp CountItemNum.cpp -o main.exe

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
  * @Author: LetMeFly
  * @Date: 2022-03-16 23:20:32
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-03-16 23:20:32
+ * @LastEditTime: 2022-03-16 23:30:55
  */
+// g++ main.cpp CountItemNum.cpp -o main.exe
+
 #include "LetMeFly.h"
 
 int main() {
