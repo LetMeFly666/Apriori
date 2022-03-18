4ea8c75094e7138e5aef1027d5df250e7db99026
LetMeFly666 <814114971@qq.com>
2022-03-18 11:05:00 +0800

    确定了没有[0，16470)之外的数据

--- CountItemNum.cpp
+++ CountItemNum.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2022-03-16 23:18:11
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-03-16 23:29:01
+ * @LastEditTime: 2022-03-18 11:03:28
  */
 #include "LetMeFly.h"
 
@@ -12,6 +12,8 @@ int countItemNum() {
     int t;
     while (~scanf("%d", &t)) {
         se.insert(t);
+        if (t < 0 || t >= 16470)
+            cout << t << endl;
     }
     return se.size();
 }
