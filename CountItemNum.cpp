d5f03ad31b1680ff789a9835a54d80dba4e60ae6
LetMeFly666 <814114971@qq.com>
2022-03-18 11:35:24 +0800

    统计个数

--- CountItemNum.cpp
+++ CountItemNum.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2022-03-16 23:18:11
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-03-18 11:03:28
+ * @LastEditTime: 2022-03-18 11:20:41
  */
 #include "LetMeFly.h"
 
@@ -10,10 +10,13 @@ int countItemNum() {
     set<int> se;
     freopen("Source/retail.dat", "r", stdin);
     int t;
+    int cnt = 0;
     while (~scanf("%d", &t)) {
         se.insert(t);
         if (t < 0 || t >= 16470)
             cout << t << endl;
+        cnt++;
     }
+    dbg(cnt);
     return se.size();
 }
--- LetMeFly.h
+++ LetMeFly.h
@@ -2,11 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2022-03-16 23:18:30
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-03-16 23:27:47
+ * @LastEditTime: 2022-03-18 11:18:41
  */
 #pragma once
 #include <iostream>
 #include <set>
+#define dbg(x) cout << #x << " = " << x << endl
 using namespace std;
 
 int countItemNum();
