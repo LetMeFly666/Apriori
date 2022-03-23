c3ba1c815c11dbcf0f693ddc4fb11be82c76ac83
LetMeFly666 <814114971@qq.com>
2022-03-23 08:42:41 +0800

    修改CountItemNum.cpp中set为unordered_set，因为未include set

--- CountItemNum.cpp
+++ CountItemNum.cpp
@@ -2,12 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2022-03-16 23:18:11
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-03-18 11:20:41
+ * @LastEditTime: 2022-03-23 08:41:51
  */
 #include "LetMeFly.h"
 
 int countItemNum() {
-    set<int> se;
+    unordered_set<int> se;
     freopen("Source/retail.dat", "r", stdin);
     int t;
     int cnt = 0;
